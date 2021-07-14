4f1bc8ef878cb9fe759eff52a63cd1fa79fdfb29
TruongGiaPhu <85993254+TruongGiaPhu@users.noreply.github.com>
2021-07-14 15:15:20 +0700

    tuần 3

--- BigSchool/BigSchool/Controllers/CoursesController.cs
+++ BigSchool/BigSchool/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using BigSchool.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -49,5 +50,23 @@ namespace BigSchool.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var cousers = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = cousers,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
+        }
+     
     }
 }
